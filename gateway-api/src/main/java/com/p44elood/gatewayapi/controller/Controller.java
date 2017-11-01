649b68ec8b7a01d5968654363bb7c109d75cebbe
P44elovod <eug.kovtun@gmail.com>
2017-11-02 01:04:46 +0200

    добавлены методы getQuery и error

--- gateway-api/src/main/java/com/p44elood/gatewayapi/controller/Controller.java
+++ gateway-api/src/main/java/com/p44elood/gatewayapi/controller/Controller.java
@@ -2,15 +2,26 @@ package com.p44elood.gatewayapi.controller;
 
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
 @RestController
-@EnableHystrix
 public class Controller {
 
+
+
+
+
+    @PostMapping(value = "/gateway-api")
+    public @ResponseBody
+    void addNewLink(@RequestParam String link, String key, Date expiration_date) {
+
+
+       System.out.println("sfsgdgdfgdfg");
+
+
+    }
 
     @GetMapping(value = "/gateway-api")
     public String gateWayGeneralInformation() {
@@ -18,22 +29,14 @@ public class Controller {
         return "Сервис формирования сокращенных ссылок";
     }
 
-
-    String erroreResponse() {
-        return "сервис временно недоступен";
-    }
-
-    @HystrixCommand(fallbackMethod = "erroreResponse")
-    @PostMapping(value = "/gateway-api")
-
-    public @ResponseBody
-    void addNewLink(@RequestParam String link, String key, Date expiration_date) {
-
-
-        System.out.println("sfsgdgdfgdfg");
-
-
+    @HystrixCommand(fallbackMethod = "error")
+    @GetMapping(value = "/gateway-api/{link}")
+    public String getQuery(@PathVariable("link") String link) {
+        throw new RuntimeException();
     }
 
 
+    String error(String string) {
+        return "Service is unavailable now!";
+    }
 }
