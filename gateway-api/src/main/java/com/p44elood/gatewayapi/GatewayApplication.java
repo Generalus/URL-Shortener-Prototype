895b68680a474e208aa0a436be57f812ca880d8d
P44elovod <eug.kovtun@gmail.com>
2017-11-02 23:00:46 +0200

    опытка организовать hystrix таблицу

--- gateway-api/src/main/java/com/p44elood/gatewayapi/GatewayApplication.java
+++ gateway-api/src/main/java/com/p44elood/gatewayapi/GatewayApplication.java
@@ -1,21 +1,26 @@
 package com.p44elood.gatewayapi;
 
 
-import com.p44elood.gatewayapi.configuration.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@RibbonClient(name = "gateway-api", configuration = Configuration.class)
+@ComponentScan
+@EnableHystrixDashboard
+@EnableHystrix
+
+
+//@RibbonClient(name = "gateway-api", configuration = Configuration.class)
 public class GatewayApplication {
 
 
-
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 }
